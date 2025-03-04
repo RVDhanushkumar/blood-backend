e7fbd728304da7e30a2af0ec09aed91260a0ef05
Vishnu Pavali <yoursvishnation@gmail.com>
2025-03-04 09:41:24 +0530

    fixing

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.js"
+import App from "./app.js"
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import ErrorBoundary from "./ErrorBoundary";
