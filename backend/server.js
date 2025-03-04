2acfb6e3b2f0f69b4ea9e542c5a6336af05dbca6
Vishnu Pavali <yoursvishnation@gmail.com>
2025-03-04 09:34:21 +0530

    fixing

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,29 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
+
+// CORS Configuration: Allow only your frontend's URL
+const corsOptions = {
+  origin: "https://anesblood.vercel.app", // Frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+};
+
+app.use(cors(corsOptions)); // Apply CORS configuration
+
+// Your other middleware and routes...
+app.use(express.json());
 const dbConnection = require('./database/db');
 const userRoutes = require('./routes/user-routes');
 
-app.use(express.json());
-app.use(cors());
+// DB connection
+dbConnection();
 
-// db connect
-dbConnection()
-
+// User routes
 app.use("/user", userRoutes);
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
