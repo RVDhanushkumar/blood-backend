a541fc59af69bd88a8548e6e46c6c626aa649a0f
Vishnu Pavali <yoursvishnation@gmail.com>
2025-03-04 10:37:48 +0530

    fuck me

--- backend/server.js
+++ backend/server.js
@@ -3,26 +3,27 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 
-// CORS Configuration: Allow only your frontend's URL
+// CORS Configuration: Allow all origins
 const corsOptions = {
-  origin: "https://anesblood.vercel.app", // Frontend URL
+  origin: "*", // Allow all origins
   methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
-  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+  allowedHeaders: ["Content-Type", "Authorization", "x-access-token"], // Allowed headers
 };
 
 app.use(cors(corsOptions)); // Apply CORS configuration
 
-// Your other middleware and routes...
-app.use(express.json());
-const dbConnection = require('./database/db');
-const userRoutes = require('./routes/user-routes');
+// Your other middleware
+app.use(express.json()); // For parsing application/json
 
 // DB connection
-dbConnection();
+const dbConnection = require('./database/db');
+dbConnection(); // Connect to the database
 
 // User routes
+const userRoutes = require('./routes/user-routes');
 app.use("/user", userRoutes);
 
+// Setting up the server port
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
