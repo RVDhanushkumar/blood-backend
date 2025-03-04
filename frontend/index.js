60943e950b02296b3f5e387486f7b2a672a06470
Vishnu Pavali <yoursvishnation@gmail.com>
2025-03-04 09:39:36 +0530

    fixing

--- frontend/index.js
+++ frontend/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.js"
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import ErrorBoundary from "./ErrorBoundary";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Router>
-        <App />
+        <ErrorBoundary>
+            <App />
+        </ErrorBoundary>
     </Router>
 );
