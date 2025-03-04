19d0c504800bfd83404048888520843ae606fc2e
Vishnu Vardhan <123796183+pvcube66@users.noreply.github.com>
2025-03-04 09:46:53 +0530

    Create app.js

---
+++ frontend/app.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import Home from "./components/Home";
+import Header from "./components/Header";
+
+import Register from "./components/Register";
+import UserTable from "./components/UserTable";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/getAllData" element={<UserTable />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
