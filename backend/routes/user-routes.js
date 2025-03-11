a788f3d790360290cac84b057d851c2bf38775b4
Vishnu Pavali <yoursvishnation@gmail.com>
2025-03-11 11:41:28 +0530

    editing

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const { addUser, getAllEntries, getEntryByGroup, editUser } = require("../controllers/user-controllers");
-const userValidation = require("../middleware/userValidation");
-
-const router = express.Router();
+const express=require('express');
+ const router=express.Router();
+ const { getAllEntries,getEntryByGroup,addUser}=require('../controllers/user-controllers')
+ const userLimiter=require('../middleware/rateLimiter');
+// const router = express.Router();
 
 router.post('/register',userLimiter,addUser);
  router.get('/getAllEntries',getAllEntries)
